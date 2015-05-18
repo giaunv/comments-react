53f0cd5ca58114706adb9efd9246789507123435
Giau Ngo <giaunv@gmail.com>
2015-05-18 14:56:09 +0700

    Using props

--- app/scripts/comments.js
+++ app/scripts/comments.js
@@ -1,5 +1,22 @@
 "use strict";
 
+var Comment = React.createClass({
+	displayName: "Comment",
+
+	render: function render() {
+		return React.createElement(
+			"div",
+			{ className: "comment" },
+			React.createElement(
+				"h2",
+				{ className: "commentAuthor" },
+				this.props.author
+			),
+			this.props.children
+		);
+	}
+});
+
 var CommentList = React.createClass({
 	displayName: "CommentList",
 
